18f3795da86f38d86c9d02453e7a03761d09261a
Stefan Englmeier <stef.engl.se@gmail.com>
2017-05-17 17:31:06 +0200

    Setup Components: Login, Greeting; Setup Authenticationservice

--- Code/src/app/app.module.ts
+++ Code/src/app/app.module.ts
@@ -1,20 +1,27 @@
+import { AuthenticationService } from './authentication.service';
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 
 import { AppComponent } from './app.component';
+import { LoginComponent } from './login/login.component';
+import { GreetingComponent } from './greeting/greeting.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    GreetingComponent
   ],
   imports: [
     BrowserModule,
     FormsModule,
     HttpModule
   ],
-  providers: [],
+  providers: [
+    AuthenticationService
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }

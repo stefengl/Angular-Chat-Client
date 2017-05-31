8b4e1acd63397f92fc69a8dd43f60ebf00bb428f
Benjamin <benjamin.klam@t-online.de>
2017-05-31 14:54:40 +0200

    start implement Message-History

--- Code/src/app/chat-message-history/chat-message-history.component.ts
+++ Code/src/app/chat-message-history/chat-message-history.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import {Message} from "../models/message";
+
 
 @Component({
   selector: 'app-chat-message-history',
@@ -6,6 +8,24 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./chat-message-history.component.css']
 })
 export class ChatMessageHistoryComponent implements OnInit {
+  user : string = "Benjamin";
+  messages : any[] = [
+    {
+      message: "Benny ist so cool",
+      username: "Alex Tarasov",
+      date: "31.05.2017 - 12:05"
+    },
+    {
+      message: "Schinken",
+      username: "Stefan",
+      date: "31.05.2017 - 12:06"
+    },
+    {
+      message: "Sebi stinkt",
+      username: "Benjamin",
+      date: "31.05.2017 - 12:07"
+    }
+  ];
 
   constructor() { }
 
